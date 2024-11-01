7527f6b2a58c1534db4440a3b17486b3dd65435a
Rizvi Fazi <rizvifazi124@gmail.com>
2024-11-01 23:55:52 +0530

    Initialized with main method

---
+++ netportal/src/main/java/com/banking/netportal/BankingApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+package com.banking.netportal;
+
+
+@SpringBootApplication
+public class BankingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BankingApplication.class, args);
+  }
+}
